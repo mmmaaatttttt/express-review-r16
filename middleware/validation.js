7d41f16740bb3e9f3694a292354425d78f60ccb3
Matt Lane <lane.matthew@gmail.com>
2020-06-26 17:23:16 -0700

    middleware refactor 1

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,21 +4,15 @@ const instructorSchema = require("../schemas/instructorSchema");
 const ExpressError = require("../expressError");
 
 function ensureValidCourse(req, res, next) {
-  const result = jsonschema.validate(req.body, courseSchema);
-
-  if (!result.valid) {
-    // pass validation errors to error handler
-    //  (the "stack" key is generally the most useful)
-    let listOfErrors = result.errors.map(error => error.stack);
-    let error = new ExpressError(listOfErrors, 400);
-    return next(error);
-  }
-
-  return next();
+  _validationHelper(courseSchema, req, res, next);
 }
 
 function ensureValidInstructor(req, res, next) {
-  const result = jsonschema.validate(req.body, instructorSchema);
+  _validationHelper(instructorSchema, req, res, next);
+}
+
+function _validationHelper(schema, req, res, next) {
+  const result = jsonschema.validate(req.body, schema);
 
   if (!result.valid) {
     // pass validation errors to error handler
